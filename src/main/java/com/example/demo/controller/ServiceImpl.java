0ae2348495379857197bd86d95da663ca8ba8037
Tejas <tejasvalame@gmail.com>
2025-02-02 18:33:06 +0530

    Moved annotations for better readability

--- src/main/java/com/example/demo/controller/ServiceImpl.java
+++ src/main/java/com/example/demo/controller/ServiceImpl.java
@@ -1,28 +1,22 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Component;
 
 import com.example.demo.model.Hello;
 
-import jakarta.jws.WebMethod;
-import jakarta.jws.WebParam;
-import jakarta.jws.WebService;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RestController
-@WebService(serviceName = "helloService", portName = "helloPort", targetNamespace = "com.example.demo")
+@Component
 public class ServiceImpl implements Service {
 
     @Override
-    @WebMethod()
-    @RequestMapping(path = "hello", method = RequestMethod.GET)
-    public Hello getHello(@RequestParam(defaultValue = "World") @WebParam(name="name") String name) {
+    public Hello getHello(String name) {
+
         Hello greeting = new Hello(name);
+
         log.info("Returning response: {}", greeting);
+
         return greeting;
     }
 
